d6cd3845e998a6b49a8ea1b2e7cb5fa43301e2dc
PritamDhobale <pritamdhobale2000@gmail.com>
2025-08-11 13:02:05 +0530

    Trigger GitHub deployment

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -72,7 +72,7 @@ export function FileUpload({
     const i = Math.floor(Math.log(bytes) / Math.log(k))
     return Number.parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i]
   }
-
+//return part
   return (
     <div className={`space-y-4 ${className}`}>
       {/* Upload Area */}
